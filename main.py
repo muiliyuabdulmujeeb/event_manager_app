bdfddf0d68751aade6758210516768c62fb265d5
Muiliyu Abdul-mujeeb <Muiliyuabdulmujeeb@gmail.com>
2025-06-29 12:51:28 +0100

    connect the event router and speaker router to the main file

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from routes.user import user_router 
+from routes.event import event_router
+from routes.speaker import speaker_router
 
 app = FastAPI()
 app.include_router(user_router, prefix="/user", tags=["user"])
+app.include_router(event_router, prefix="/event", tags=["event"])
+app.include_router(speaker_router, prefix="/speaker", tags=["speaker"])
 
