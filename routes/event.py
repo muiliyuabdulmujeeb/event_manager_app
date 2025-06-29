1bc93df3b54b350c2f4872aa861a26d217b0b6b5
Muiliyu Abdul-mujeeb <Muiliyuabdulmujeeb@gmail.com>
2025-06-29 13:17:56 +0100

    create the event routes with the right status codes

---
+++ routes/event.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, status
+from schemas.event import EventCreate, EventUpdate, EventOpen, EventClose
+from services.event import event_logic
+
+event_router = APIRouter()
+
+@event_router.post("/", status_code= status.HTTP_201_CREATED)
+def create_event(event_details: EventCreate):
+    return event_logic.create_event(event_details=event_details)
+
+@event_router.patch("/", status_code= status.HTTP_200_OK)
+def update_event(update_details: EventUpdate):
+    return event_logic.update_event(update_details= update_details)
+
+@event_router.get("/", status_code= status.HTTP_200_OK)
+def get_all_events():
+    return event_logic.get_all_events()
+
+@event_router.get("/{event_id}", status_code= status.HTTP_200_OK)
+def get_event_by_id(event_id: str):
+    return event_logic.get_event_by_id(event_id= event_id)
+
+@event_router.delete("/{event_id}")
+def delete_event(event_id: str):
+    return event_logic.delete_event(event_id= event_id)
+
+@event_router.patch("/close")
+def close_event_registration(event_id: EventClose):
+    return event_logic.close_event_registration(event_id= event_id)
+
+@event_router.patch("/open")
+def open_event_registration(event_id: EventOpen):
+    return event_logic.open_event_registration(event_id= event_id)
