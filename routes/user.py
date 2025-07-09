d5339744cb3d5b0bbdd05cae3cbfcec16768fa53
Muiliyu Abdul-mujeeb <Muiliyuabdulmujeeb@gmail.com>
2025-07-09 12:38:19 +0100

    refactor the code to meet up with REST standard by using the correct URIs

--- routes/user.py
+++ routes/user.py
@@ -21,13 +21,13 @@ def get_user_by_id(user_id: str):
 def update_user(to_update: UserUpdate):
    return user_logic.update_user(to_update=to_update)
 
-@user_router.patch("/deactivate", status_code= status.HTTP_200_OK)
-def deactivate_user(to_deactivate: UserDeactivate):
-   return user_logic.deactivate_user(to_deactivate=to_deactivate)
+@user_router.patch("/{user_id}/deactivate", status_code= status.HTTP_200_OK)
+def deactivate_user(user_id: str):
+   return user_logic.deactivate_user(user_id= user_id)
 
-@user_router.patch("/activate", status_code= status.HTTP_200_OK)
-def activate_user(to_activate: UserActivate):
-   return user_logic.activate_user(to_activate=to_activate)
+@user_router.patch("/{user_id}/activate", status_code= status.HTTP_200_OK)
+def activate_user(user_id: str):
+   return user_logic.activate_user(user_id= user_id)
 
 @user_router.delete("/{user_id}", status_code= status.HTTP_200_OK)
 def delete_user(user_id: str):
