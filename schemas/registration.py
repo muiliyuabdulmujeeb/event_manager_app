8ef428bacbf023c03045266264e2ca21d0a8e714
Muiliyu Abdul-mujeeb <Muiliyuabdulmujeeb@gmail.com>
2025-07-12 09:38:09 +0100

    create registration schema

---
+++ schemas/registration.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from datetime import date
+
+#Registration Parameters
+#1. id
+#2. user_id
+#3. event_id
+#4. registration_date
+#5. attended (Default is false)
+
+class RegistrationCreate(BaseModel):
+    user_id: str
+    event_id: str
+    registration_date: date
+
+class MarkAttendance(BaseModel):
+    id: str
