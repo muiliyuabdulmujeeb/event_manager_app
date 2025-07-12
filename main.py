07887104106baa5ca787ecf222acdf92e56b1a90
Muiliyu Abdul-mujeeb <Muiliyuabdulmujeeb@gmail.com>
2025-07-12 09:42:35 +0100

    add the registration routes to the main

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from routes.user import user_router 
 from routes.event import event_router
 from routes.speaker import speaker_router
+#from routes.registration import registration_router
 
 app = FastAPI()
 app.include_router(user_router, prefix="/user", tags=["user"])
 app.include_router(event_router, prefix="/event", tags=["event"])
 app.include_router(speaker_router, prefix="/speaker", tags=["speaker"])
+#app.include_router(registration_router, prefix="/register", tags=["registration"])
 
