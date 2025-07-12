bf1db3229306d2667bd4096e1bc5dbe2e938b41e
Muiliyu Abdul-mujeeb <Muiliyuabdulmujeeb@gmail.com>
2025-07-12 09:40:03 +0100

    add the event_speaker list to  database

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
-from models import User, Speaker, Event, Registration
+from models import User, Speaker, Event, EventSpeaker, Registration
 
 users: list[User] = []
 speakers: list[Speaker] = []
+event_speakers: list[EventSpeaker] = []
 events: list[Event] = []
 registrations: list[Registration] = []
