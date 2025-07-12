bba73812e2a983973d0faf781a8de41820a14ee2
Muiliyu Abdul-mujeeb <Muiliyuabdulmujeeb@gmail.com>
2025-07-12 09:45:02 +0100

    create the event_speaker model

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 class User:
     def __init__(self, id: str, name: str, email: str, is_active: bool = True):
         self.id = id
@@ -12,12 +14,21 @@ class Speaker:
         self.topic = topic
 
 class Event:
-    def __init__(self, id: str, title: str, location: str, date: str, is_open: bool = True):
+    def __init__(self, id: str, title: str, location: str, date: str, event_speaker: Optional[list]= None, is_open: bool = True):
         self.id = id
         self.title = title
         self.location = location
         self.date = date
         self.is_open = is_open
+        if event_speaker is None:
+            event_speaker = []
+        self.eventspeaker = event_speaker
+
+class EventSpeaker:
+    def __init__(self, id: str, event_id: str, speaker_id: str):
+        self.id = id
+        self.event_id = event_id
+        self.speaker_id = speaker_id
 
 class Registration:
     def __init__(self, id: str, user_id: str, event_id: str, date: str, attended: bool = False):
