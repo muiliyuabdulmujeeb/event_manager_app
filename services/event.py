a04a25edc3233add32f1a9d0644d7f3ef382f565
Muiliyu Abdul-mujeeb <Muiliyuabdulmujeeb@gmail.com>
2025-07-12 09:55:50 +0100

    create eventspeaker CRUD

--- services/event.py
+++ services/event.py
@@ -1,11 +1,13 @@
 from fastapi import HTTPException, status
 import uuid
-from database import events
+from database import events, speakers, event_speakers
 from models import Event as EventModel
-from schemas.event import EventCreate, EventUpdate
+from models import EventSpeaker as EventSpeakerModel
+from schemas.event import EventCreate, EventUpdate, EventSpeaker
 
 class EventLogic:
 
+    #event CRUD
     @staticmethod
     def create_event(event_details: EventCreate):
         event_id = str(uuid.uuid4())
@@ -57,5 +59,65 @@ class EventLogic:
                 event.is_open = True
                 return {"Message": "Event registration opened"}
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Event not found")
+    
+    #event_speaker CRUD
+    @staticmethod
+    def create_event_speaker(event_speaker_details: EventSpeaker):
+
+        #check if speaker exists
+        #global speaker_present
+        speaker_present = False
+        for speaker in speakers:
+            if speaker.id == event_speaker_details.speaker_id:
+                speaker_present = True
+                break
+        if not speaker_present:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Speaker does not exist, Use initialized Speaker")
+        
+        #check if event exists and append the event_speaker_data to event_data
+        #event_present
+        event_present = False
+        for event in events:
+            if event.id == event_speaker_details.event_id:
+                event_present = True
+                #handle the event speaker data
+                event_speaker_id = str(uuid.uuid4())
+                event_speaker_data = EventSpeakerModel(event_speaker_id, **event_speaker_details.model_dump())
+                event_speakers.append(event_speaker_data)
+                (event.eventspeaker).append(event_speaker_data)
+                break
+        if not event_present:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Event not found")
+        
+        
+        return event_speaker_data
+ 
+    @staticmethod
+    def get_all_event_speakers():
+        return event_speakers
+    
+    @staticmethod
+    def get_event_speaker_by_id(event_speaker_id: str):
+        for event_speaker in event_speakers:
+            if event_speaker.id == event_speaker_id:
+                return event_speaker
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Event speaker not found")
+    
+    @staticmethod
+    def delete_event_speaker(event_speaker_id: str):
+        #delete the event speaker in the event_speaker database
+        for event_speaker in event_speakers:
+            if event_speaker.id == event_speaker_id:
+                event_speakers.remove(event_speaker)
+
+         #delete the event speaker in the events database      
+        for event in events:
+            for speaker in event.eventspeaker:
+                if speaker.id == event_speaker_id:
+                    (event.eventspeaker).remove(speaker)
+                    return {"Message": "Event speaker deleted"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Event speaker not found")
+    
+
 
 event_logic = EventLogic()
