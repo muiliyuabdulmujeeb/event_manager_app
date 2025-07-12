85b527c098bd2f5225c27933e60955f8c4c13797
Muiliyu Abdul-mujeeb <Muiliyuabdulmujeeb@gmail.com>
2025-07-12 09:47:22 +0100

    create the event_speaker routes

--- routes/event.py
+++ routes/event.py
@@ -1,9 +1,27 @@
 from fastapi import APIRouter, status
-from schemas.event import EventCreate, EventUpdate
+from schemas.event import EventCreate, EventUpdate, EventSpeaker
 from services.event import event_logic
 
 event_router = APIRouter()
 
+#event speaker routes
+@event_router.post("/eventspeaker", status_code= status.HTTP_201_CREATED)
+def create_event_speaker(event_speaker_details: EventSpeaker):
+    return event_logic.create_event_speaker(event_speaker_details=event_speaker_details)
+
+@event_router.get("/eventspeaker", status_code= status.HTTP_200_OK)
+def get_all_event_speakers():
+    return event_logic.get_all_event_speakers()
+
+@event_router.get("/eventspeaker/{event_speaker_id}", status_code= status.HTTP_200_OK)
+def get_event_speaker_by_id(event_speaker_id: str):
+    return event_logic.get_event_speaker_by_id(event_speaker_id= event_speaker_id)
+
+@event_router.delete("/eventspeaker/{event_speaker_id}", status_code= status.HTTP_200_OK)
+def delete_event_speaker(event_speaker_id: str):
+    return event_logic.delete_event_speaker(event_speaker_id= event_speaker_id)
+
+#event routes
 @event_router.post("/", status_code= status.HTTP_201_CREATED)
 def create_event(event_details: EventCreate):
     return event_logic.create_event(event_details=event_details)
@@ -20,7 +38,7 @@ def get_all_events():
 def get_event_by_id(event_id: str):
     return event_logic.get_event_by_id(event_id= event_id)
 
-@event_router.delete("/{event_id}")
+@event_router.delete("/{event_id}", status_code= status.HTTP_200_OK)
 def delete_event(event_id: str):
     return event_logic.delete_event(event_id= event_id)
 
